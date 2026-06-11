acd10dc45af9a1bec0f2910407d71e1d94d51f6a
Meterius <Meterius@users.noreply.github.com>
2026-06-11 17:14:02 +0200

    fix terrain color mesh render layer

--- crates/jlh_maps_app/src/app/map/terrain.rs
+++ crates/jlh_maps_app/src/app/map/terrain.rs
@@ -1,6 +1,6 @@
 use crate::app::common::debug_gizmos::DebugAabbGizmo;
 use crate::app::common::materials::TransparentOverwriteMaterial;
-use crate::app::map::core::{MAP_VIEW_COLOR_RENDER_LAYER, MAP_VIEW_NON_TERRAIN_RENDER_LAYER};
+use crate::app::map::core::{MAP_VIEW_COLOR_RENDER_LAYER, MAP_VIEW_NON_TERRAIN_RENDER_LAYER, MAP_VIEW_TERRAIN_RENDER_LAYER};
 use crate::app::map::transform::MERCATOR_WORLD_SIZE;
 use crate::app::maplibre_gl_js::types::{CanonicalTileId, MlTerrain, MlTerrainTile};
 use crate::app::maplibre_gl_js::utils::mercator_coordinate::{
@@ -124,7 +124,7 @@ fn sync_spawned_tiles(
                         Mesh3d(flat_mesh.0.clone()),
                         MeshMaterial3d(materials.color_material.clone()),
                         DebugAabbGizmo,
-                        RenderLayers::from_layers(&[MAP_VIEW_COLOR_RENDER_LAYER]),
+                        RenderLayers::from_layers(&[MAP_VIEW_TERRAIN_RENDER_LAYER]),
                     ))
                     .id();
 
